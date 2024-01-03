e401b063971acbe95cfb9cc1375a0a87a6531324
Kokiben <bennassarkaoutar@gmail.com>
2024-01-03 11:49:15 -0800

    last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_last_digit(num):
+    if num < 0:
+        num = abs(num)
+        print(num % 10, end="")
+    else:
+        print(num % 10, end="")
+    return (num % 10)
