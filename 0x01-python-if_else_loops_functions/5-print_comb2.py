2b70ddaa818c4fc5368df55de5d5cd0ae1c2176c
Kokiben <bennassarkaoutar@gmail.com>
2024-01-03 14:25:35 -0800

    pt

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for n in range(00, 100):
-    print("{:02d}".format(n), end='\n' if n == 99 else ", ")
+for n in range(0, 99):
+    print('{:02d}'.format(n), end=' ')
+print('99')
