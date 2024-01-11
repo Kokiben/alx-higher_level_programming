77d80552ee3029474f6653f5023c63c9af1347f4
Kokiben <bennassarkaoutar@gmail.com>
2024-01-10 17:11:26 -0800

    py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,28 +1,16 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or not isinstance(roman_string, str):
+    roman_diccti = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    n = 0
+
+    if roman_string is None or type(roman_string) is not str:
         return 0
 
-    roman_int = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
-
-    deci = 0
-    previ_val = 0
-
-    for num in reversed(roman_string):
-        val = roman_int.get(num, 0)
-        if val == 0:
-            return 0
-        if val < previ_val:
-            deci -= val
+    for a in range(len(roman_string)):
+        if a == len(roman_string) - 1:
+            n += roman_diccti[roman_string[a]]
+        elif roman_diccti[roman_string[a]] >= roman_dicc[roman_string[a + 1]]:
+            n += roman_diccti[roman_string[a]]
         else:
-            deci += val
-        previ_val = val
-   return deci
+            n -= roman_diccti[roman_string[a]]
+    return n
