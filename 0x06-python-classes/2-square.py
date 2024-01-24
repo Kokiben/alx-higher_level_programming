e39b74d05d7f1f99c89735362a887970c174c86c
Kokiben <bennassarkaoutar@gmail.com>
2024-01-23 16:53:36 -0800

    sq

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Class definition for a Square."""
+
+
+class Square:
+    """A simple represention of a square."""
+
+    def __init__(self, side_leng):
+        """Initialize a new Square with a specified side leng.
+
+        Args:
+            side_leng (int): The side leng of the new square.
+        """
+        if type(side_leng) is not int:
+            raise TypeError("side_leng must be an integer")
+        if side_leng < 0:
+            raise ValueError("side_leng must be >= 0")
+        self.__size = side_leng
