b4851c9dc92a0f720ee272784e584ad784a44d22
Kokiben <bennassarkaoutar@gmail.com>
2024-01-24 15:32:13 -0800

    sq

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""Class definition for a Square"""
+
+
+class Square:
+    """Initialize a new Square with a specified size."""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, val):
+        if type(val) is not int:
+            raise TypeError("size must be an integer")
+        elif val < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for a in range(self.__size):
+            for b in range(self.__size):
+                print("#", end="")
+            print()
