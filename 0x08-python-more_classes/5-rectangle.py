872a571896096a33de58e2c57b1367a27fcabc1d
Kokiben <bennassarkaoutar@gmail.com>
2024-01-29 10:33:05 -0800

    rec

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+"""
+    Class definition for a Rectangle
+"""
+
+
+class Rectangle:
+    """
+        Initialze the rectangle a specified width and height
+        square
+    """
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """
+            Retrieve value of width square
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+            Modify value of width square
+        """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+            Retrieve value of height square
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+            Modify value of height square
+        """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """
+            Retrieve area width multiplied by height
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+            Retrieve perimeter width Plus height
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return self.__width * 2 + self.__height * 2
+
+    def __str__(self):
+        """
+            __str__
+        """
+        me_rect = ""
+        if self.__height == 0 or self.__width == 0:
+            return me_rect
+        for a in range(self.__height):
+            for k in range(self.__width):
+                me_rect += '#'
+            if a < self.__height - 1:
+                me_rect += '\n'
+        return me_rect
+
+    def __repr__(self):
+        """Return str"""
+        me_rect = "Rectangle(" + str(self.__width)
+        me_rect += ", " + str(self.__height) + ")"
+        return (me_rect)
+
+     def __del__(self):
+        """Print a msg"""
+        print("Bye rectangle...")
