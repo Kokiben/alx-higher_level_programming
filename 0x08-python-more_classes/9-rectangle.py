51cb63a9142bd861506381fa45c38b9c45458833
Kokiben <bennassarkaoutar@gmail.com>
2024-01-29 16:50:07 -0800

    rect

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -110,7 +110,7 @@ class Rectangle:
         """
         print("Bye rectangle...")
         type(self).number_of_instances -= 1
-
+        
     @classmethod
     def square(cls, size=0):
         return cls(size, size)
