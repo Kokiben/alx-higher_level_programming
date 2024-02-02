55ba6b7551c345c6577f4b881ba4d3c01dc6deda
Kokiben <bennassarkaoutar@gmail.com>
2024-02-02 10:48:56 -0800

    matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 """
-Defines a matrix division function.
-that divides all elements of a matrix.
+Define function that divides all elements of a matrix.
 """
 
 
 def matrix_divided(matrix, div):
-    """ Divide all elements of a matrix.
+    """Divides all elements in the matrix by div.
 
     Args:
-    matrix (list): A list of lists of ints or floats.
-    div (int/float): The divisor.
+    matrix : List of lists of integers/floats.
+    div : The Factor.
     Raises:
-    TypeError: If the matrix contains non-numbers.
-    TypeError: If the matrix contains rows of different sizes.
-    TypeError: If div is not an int or float.
-    ZeroDivisionError: If div is 0.
+    TypeError: If matrix includes non-numbers.
+    TypeError: If matrix includes rows of different sizes.
+    TypeError: If divide is not an integer or float.
+    ZeroDivisionError: If divide is 0.
 
     Returns:
-    A new matrix representing the result of the division.
+    A new matrix.
     """
     if matrix is None:
         raise TypeError(
@@ -28,8 +27,8 @@ def matrix_divided(matrix, div):
         if not isinstance(row, list):
             raise TypeError(
                 "matrix must be a matrix (list of lists) of integers/floats")
-        for element in row:
-            if not (isinstance(element, int) or isinstance(element, float)):
+        for node in row:
+            if not (isinstance(node, int) or isinstance(node, float)):
                 raise TypeError(
                     "matrix must be a matrix (list of lists) "
                     "of integers/floats")
@@ -42,11 +41,11 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    divided_matrix = []
+    matrix_div = []
     for row in matrix:
-        divided_row = []
-        for element in row:
-            result = round(element / div, 2)
-            divided_row.append(result)
-        divided_matrix.append(divided_row)
-    return divided_matrix
+        row_mat = []
+        for node in row:
+            sut = round(node / div, 2)
+            row_mat.append(sut)
+        matrix_div.append(row_mat)
+    return matrix_div
