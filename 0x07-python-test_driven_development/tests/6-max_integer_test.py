71c920d26c2511858cc509ec9703abd46163dcd4
Kokiben <bennassarkaoutar@gmail.com>
2024-02-02 17:44:45 -0800

    max

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -33,13 +33,13 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_positive_end(self):
         """Test all positive with max at end"""
-        i = [2, 10, 8, 36, 14, 50]
-        self.assertEqual(max_integer(i), 50)
+        i = [4, 10, 8, 38, 16, 60]
+        self.assertEqual(max_integer(i), 60)
 
     def test_positive_middle(self):
         """Test all positive with max in middle"""
-        n = [2, 10, 8, 360, 14, 50]
-        self.assertEqual(max_integer(n), 360)
+        n = [4, 10, 8, 380, 16, 60]
+        self.assertEqual(max_integer(n), 380)
 
     def test_positive_beginning(self):
         """Test all positive with max at beginning"""
@@ -48,12 +48,12 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_one_negative(self):
         """Test list with one negative number"""
-        nc = [200, 10, 8, -36, 14, 50]
-        self.assertEqual(max_integer(nc), 200)
+        nc = [300, 10, 8, -38, 16, 60]
+        self.assertEqual(max_integer(nc), 300)
 
     def test_all_negative(self):
-        """Tests for list with all negative numbers"""
-        j = [-6, -50, -75, -1, -100]
+        """Test for list with all negative numbers"""
+        j = [-8, -60, -75, -1, -200]
         self.assertEqual(max_integer(j), -1)
 
     def test_none(self):
@@ -62,8 +62,8 @@ class TestMaxInteger(unittest.TestCase):
             max_integer(None)
 
     def test_non_int_arg(self):
-        """Test  a non-integer type in list"""
-        sti = [1, 2, "Hello", 4, 5]
+        """Test a non-integer type in list"""
+        sti = [1, 2, "kaout", 4, 5]
         with self.assertRaises(TypeError):
             max_integer(sti)
 
