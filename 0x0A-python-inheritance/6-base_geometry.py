ca6df3b40b7aeb479263e83103f1a03c628f5414
Kokiben <bennassarkaoutar@gmail.com>
2024-02-05 10:20:25 -0800

    basegeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Establishes a blank class called BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    Illustrate fundamental geometric principles.
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
