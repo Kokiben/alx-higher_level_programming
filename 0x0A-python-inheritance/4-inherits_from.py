063bbe1b7179703987ea43d821c2e36757ca0d83
Kokiben <bennassarkaoutar@gmail.com>
2024-02-05 10:09:15 -0800

    inheritsfrom

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,7 +7,7 @@ Create a function for checking inheritance in a defined class.
 def inherits_from(obj, a_class):
     """
     Construct a function that checks if an object is an instance of a class
-    that inherited (either directly or through intermediate classes) from the specified class.
+    that inherited from the specified class.
     """
     if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
