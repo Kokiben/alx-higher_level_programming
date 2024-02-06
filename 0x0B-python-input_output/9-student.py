9b8c8b2e0483ebc7b41b066f84ce5566e45360ab
Kokiben <bennassarkaoutar@gmail.com>
2024-02-06 11:46:21 -0800

    student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Class Definition for  Student."""
+
+
+class Student:
+    """A simple represention of a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+
+        Args:
+            first_name (str): Name first of student.
+            last_name (str): Name last of student.
+            age (int): Age of student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Provide the dictionary depiction of the Student."""
+        return self.__dict__
