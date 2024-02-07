8adfa0f972fb6acc7febe5e4ea723f99eaf19f18
Kokiben <bennassarkaoutar@gmail.com>
2024-02-06 18:19:47 -0800

    student

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,5 +1,5 @@
-!/usr/bin/python3
-"""Class Definition for  Student."""
+#!/usr/bin/python3
+"""Defines a class Student."""
 
 
 class Student:
@@ -17,15 +17,23 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """Provide the dictionary depiction of the Student."""
-        return self.__dict__
+        if attrs is None:
+            return self.__dict__
+        else:
+            result = {}
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    result[key] = value
+            return result
 
     def reload_from_json(self, json):
         """Exchange all characteristics of the Student.
 
         Args:
-            json (dict): A dictionar containing key-value pairs to substitute student characteristics.
+            json (dict): A dictionary containing key/value pairs
+            to substitute student characteristics.
         """
-        for g, d in json.items():
-            setattr(self, g, d)
+        for key, value in json.items():
+            setattr(self, key, value)
