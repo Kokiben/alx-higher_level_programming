594b151edb4395726ec2a8a1beb32574208ef7eb
Kokiben <bennassarkaoutar@gmail.com>
2024-02-14 18:45:17 -0800

    rec

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
-"""Unittest for base
-"""
-
+""" unit test for bases """
 import unittest
+from models.square import Square
+from models.rectangle import Rectangle
 from models.base import Base
 
 
-class TestBase(unittest.TestCase):
-    """Define unit test for base model"""
+class BaseTestCase(unittest.TestCase):
+    """ class for base test """
+    def setUp(self):
+        """
+        Resets id
+        """
+        Base._Base__nb_objects = 0
 
-    def test_initialization(self):
-        base1 = Base()
-        base2 = Base()
-        self.assertEqual(base1.id, 1)
-        self.assertEqual(base2.id, 2)
+    def test_base_task1(self):
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
 
-    def test_saving_id(self):
-        base = Base(100)
-        self.assertEqual(base.id, 100)
+        b2 = Base()
+        b3 = Base()
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 3)
 
-    def test_to_json_string_valid(self):
-        pass
+        b4 = Base(12)
+        b5 = Base()
+        self.assertEqual(b4.id, 12)
+        self.assertEqual(b5.id, 4)
 
 
 if __name__ == '__main__':
