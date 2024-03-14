d3290a0a9bc4f492e2f06ba91d6266ae6b69af1a
Kokiben <bennassarkaoutar@gmail.com>
2024-03-13 19:01:32 -0700

    sql

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,3 @@
+-- List all cities contained in the database hbtn_0d_usa
+-- List all rows
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id;
