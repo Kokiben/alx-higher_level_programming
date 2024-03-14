962ad35341b4a2ef3cca92d9207eed710270b20f
Kokiben <bennassarkaoutar@gmail.com>
2024-03-13 19:19:07 -0700

    sql

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- List all request from hbtn_0d_tvshows and print
+-- List all rows
+SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.genre_id) AS 'number_of_shows'
+FROM tv_genres RIGHT JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
