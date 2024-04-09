d77750499f1f6a2cb1646f9eadb555f6fb4641b5
Kokiben <bennassarkaoutar@gmail.com>
2024-04-08 17:25:56 -0700

    arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const argsLength = process.argv.length;
+
+switch (argsLength) {
+    case 2:
+        console.log("No argument");
+        break;
+    case 3:
+        console.log("Argument found");
+        break;
+    default:
+        console.log("Arguments found");
+}
