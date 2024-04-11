f43a5986486629c2061c830b47f83c61a7a7fcaa
Kokiben <bennassarkaoutar@gmail.com>
2024-04-11 16:49:22 -0700

    sec

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@ if (process.argv.length <= 3) {
   console.log('0');
 } else {
   const Ser = process.argv.slice(2).map(Number);
-  const Sec = Ser.sort(function (c, d) { return c - d; })[1];
+  const Sec = Ser.sort(function (a, b) { return b - a; })[1];
   console.log(Sec);
 }
