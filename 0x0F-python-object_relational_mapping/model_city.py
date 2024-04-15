66b26a6b812d60c3ed9d58c60e5ee9fb39c38648
Kokiben <bennassarkaoutar@gmail.com>
2024-04-14 19:30:28 -0700

    city

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -6,6 +6,7 @@ from model_state import Base
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
 
+
 class City(Base):
     """
     A simple represention of a class city
