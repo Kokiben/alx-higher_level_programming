1956a5f01ad6d71c72df09d1faf4712ac1e3d0ab
Kokiben <128861864+Kokiben@users.noreply.github.com>
2024-04-15 02:58:23 +0100

    Update 12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -3,7 +3,7 @@
 """
 import sys
 from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sitting
 from sqlalchemy import (create_engine)
 
 
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
-    Sitting = sessionmaker(bind=engine)
+    Sitting = sitting(bind=engine)
     sitting = Sitting()
     new_call = sitting.query(State).filter_by(id=2).first()
     new_call.name = 'New Mexico'
