0400bcc2c8107faaf2a37db118d8d6e47a2506ab
Kokiben <bennassarkaoutar@gmail.com>
2024-04-26 16:36:17 -0700

    my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,10 @@
 """Call https://alx-intranet.hbtn.io/status."""
 import urllib.request
 
-urll = "https://alx-intranet.hbtn.io/status"
-
-with urllib.request.urlopen(url) as response:
-    body = response.read().decode('utf-8')
-
-print("- Body response:")
-print("\t- type:", type(body))
-print("\t- content:", body)
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        body = response.read().decode('utf-8')
+        print("- Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body)
