7c6b302c43533e9f8910faee97de9e971630f480
Kokiben <bennassarkaoutar@gmail.com>
2024-04-26 19:19:35 -0700

    error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-""" module doc """
+""" Sends a request to URL and displays body of response """
 import urllib.request
 import sys
 import urllib.error
 
+
 if __name__ == "__main__":
     try:
         urll = sys.argv[1] # Get the URL from the command-line argument
-        # Open the URL and read
-        with urllib.request.urlopen(urll) as rps:
+        with urllib.request.urlopen(urll) as rps: # Open the URL and read
             print(rps.read().decode('utf-8'))
-    except urllib.error.HTTPError as err:
-    # Handle HTTP errors
+    except urllib.error.HTTPError as err: # Handle HTTP errors
         print(f"Error code: {err.code}"
-        # Print the HTTP status code of the error
