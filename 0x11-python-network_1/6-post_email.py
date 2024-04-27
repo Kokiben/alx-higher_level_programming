8200de198fb113a0a65de53ffad732ae3a1f06ec
Kokiben <bennassarkaoutar@gmail.com>
2024-04-26 19:38:23 -0700

    post email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Sends a POST request to the passed URL with the email as a parameter"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    urll = sys.argv[1] # Get the URL from the command-line argument
+    ema = {"email": sys.argv[2]}
+    rq = requests.post(urll, data=ema)
+
+    print(rq.text)
