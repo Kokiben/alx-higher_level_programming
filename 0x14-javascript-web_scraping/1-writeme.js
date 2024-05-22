c4127ff525ca97535f6019dab87e850842206a07
Kokiben <bennassarkaoutar@gmail.com>
2024-05-21 17:58:50 -0700

    txt

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,14 +2,14 @@
 
 const filSystm = require('fs');  // Renaming 'fs' to 'fileSystem'
 const filPath = process.argv[2];  // The first argument is the file path
-const cott = process.argv[3];   // The second argument is the string to write
+const cott = process.argv.slice(3).join(' ');   // The second arg
 
 if (!filPath || !cott) {
     console.log("Usage: node 1-writeme.js <file_path> <string_to_write>");
     process.exit(1);
 }
 
-filSystm.writeFile(filPath, cott, 'utf8', (err) => {  // Writing the content to the file in UTF-8
+filSystm.writeFile(filPath, cott, 'utf8', (err) => {  // Writing UTF-8
   if (err) {
     console.error(err);  // If an error occurs, print the error object
   }
