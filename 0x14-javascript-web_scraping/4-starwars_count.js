1158bf2be416c11918b28882ff7a1bd9c5b4f5d7
Kokiben <bennassarkaoutar@gmail.com>
2024-05-21 18:57:40 -0700

    count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,39 +1,22 @@
 #!/usr/bin/node
 
 const rqt = require('request');
-
 const apUrl = process.argv[2];
+const charId = '18';
+let cont = 0;
 
-const charId = '18'; // Char ID of Wedge Antilles
-
-// Fun to count the num of movies where char is present
-function contMoviesWithChar(fms) {
-    let cont = 0;
-    fms.forEach(fm => {
-        if (fm.characters.includes(`https://swapi-api.alx-tools.com/api/people/${charId}/`)) {
-            cont++;
+rqt.get(apUrl, (err, rsps, rqtdata) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const chardata = JSON.parse(rqtdata);
+    chardata.results.forEach((movie) => {
+      movie.characters.forEach((character) => {
+        if (character.includes(charId) || character.includes(`/people/${charId}/`)) {
+          cont += 1;
         }
+      });
     });
-    return cont;
-}
-
-// Making a request to retrieve information about each film
-rqt(apUrl, (err, rsps, rpsdata) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    
-    console.log(rsps); // Log the response object
-
-    if (rsps.statusCode !== 200) {
-        console.error(`Failed to retrieve data. Status code: ${rsps.statusCode}`);
-        return;
-    }
-
-    const fms = JSON.parse(rspsdata).results; // list of films from response
-
-    // Counting num of movies where char is present
-    const movCont = contMoviesWithChar(fms);
-    console.log(movCont);
+    console.log(cont);
+  }
 });
