d8f1d5abfb4aac2418a5bd1d8d851dcb0f1843e9
Kokiben <bennassarkaoutar@gmail.com>
2024-05-22 17:23:52 -0700

    read

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,5 @@
 #!/usr/bin/node
-
-const fileSystem = require('fs');  // Renaming 'fs' to 'fileSystem'
-const filePath = process.argv[2];  // Renaming 'filename' to 'filePath'
-
-fileSystem.readFile(filePath, 'utf8', (err, data) => {  // Using 'fileSystem'
-  if (err) {
-    console.log(err);  // Using 'err' instead of 'error'
-  } else {
-    console.log(data);  // Using 'data' instead of 'content'
-  }
+const fileSys = require('fs');
+fileSys.readFile(process.argv[2], 'utf8', function (err, cott) {
+  console.log(err || cott);
 });
