1dc675063243a15e09e6f22620c9cb3ab80f1d9f
Kokiben <bennassarkaoutar@gmail.com>
2024-05-22 17:39:04 -0700

    rqs

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,20 +1,8 @@
 #!/usr/bin/node
+const fileSys = require('fs');
+const rqst = require('request');
 
-const rqt = require('request');
-const filSys = require('fs');
-const apUrl = process.argv[2];
-const fl = process.argv[3]; // Keeping 'file' as is
+const apurl = process.argv[2];
+const fPath = process.argv[3];
 
-rqt(apUrl, (err, rsps, rpsdata) => {
-  if (err) {
-    console.log(err);
-  } else {
-    filSys.writeFile(fl, rpsdata, 'utf8', (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(`File "${fl}" has been saved.`);
-      }
-    });
-  }
-});
+rqst(apurl).pipe(fileSys.createWriteStream(fPath));
